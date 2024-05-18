7c66c62be1369c2c3d19edbc71304b9e99a6746c
Tyler Fahey <tyler@tylerfahey.com>
2024-05-18 05:43:26 -0500

    Update config path in settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -878,6 +878,9 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
+// Put config outside app root by default
+$settings['config_sync_directory'] = '../config/sync';
+
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
